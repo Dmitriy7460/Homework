7f062283c5462702ae3fb66c930d5b8fa6c304ed
Дмитрий <matveev_dmitriy1996@mail.ru>
2022-12-21 15:08:19 +0300

    Решена задача №2(v2)

---
+++ Dz1_Zadacha2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number1 > number2) { Console.WriteLine($"max = {number1},min = {number2}"); }
+else if (number1 < number2) { Console.WriteLine($"max={number2},min={number1}");}
+else if (number1 == number2) { Console.WriteLine("Введенные значения не удовлетворяют условиям задачи.");}
+
