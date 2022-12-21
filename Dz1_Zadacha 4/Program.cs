e305a4bf48de83e30cbbb8761b1f71d2a6407e2c
Дмитрий <matveev_dmitriy1996@mail.ru>
2022-12-21 15:29:18 +0300

    Решена задача №4

---
+++ Dz1_Zadacha 4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите 3 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>a && b>c) {max=b;}
+if (c>a && c>b) {max=c;}
+
+
+Console.WriteLine($"max={max}");
