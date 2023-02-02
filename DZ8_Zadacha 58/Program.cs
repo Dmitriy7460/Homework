1c02678c37f77ee4a4878ee201cb7f7ae3f3773c
Дмитрий <matveev_dmitriy1996@mail.ru>
2023-02-02 06:55:00 +0300

    Complite homework #8

---
+++ DZ8_Zadacha 58/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+int[,] matrix1 = CreateMatrixDobleRnd(2, 1, 1, 3);  // Строки, Столбцы, Мин, Макс.
+PrintMatrix(matrix1);
+
+int[,] matrix2 = CreateMatrixDobleRnd(1, 3, 0, 3);  // Строки, Столбцы, Мин, Макс.
+PrintMatrix(matrix2);
+
+if (Check(matrix1, matrix2) == true)
+{
+    int[,] answer = Multiplication(matrix1, matrix2);
+    PrintMatrix(answer);
+}
+
+
+//\\//\\//\\//\\//\\//\\
+
+
+int[,] CreateMatrixDobleRnd(int rows, int coloms, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] mtr = new int[rows, coloms];
+
+    for (int i = 0; i < mtr.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtr.GetLength(1); j++)
+        {
+            mtr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return mtr;
+}
+
+void PrintMatrix(int[,] mtr)
+{
+    Console.WriteLine();
+
+    for (int i = 0; i < mtr.GetLength(0); i++)
+    {
+        Console.Write("|");
+
+        for (int j = 0; j < mtr.GetLength(1); j++)
+        {
+            Console.Write($"{mtr[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+bool Check(int[,] first, int[,] second)
+{
+    bool reliability = (first.GetLength(1) == second.GetLength(0));
+
+    if (reliability != true)
+    { 
+        Console.WriteLine();
+        Console.WriteLine("Число столбцов первой матрицы должо соответствовать числу строк второй матрицы."); 
+    };
+
+    return reliability;
+}
+
+int[,] Multiplication(int[,] first, int[,] second)
+{
+    int rows = first.GetLength(0);     
+    int coloms = second.GetLength(1);     
+    int universal = first.GetLength(1);
+
+    int[,] ans = new int[rows, coloms];
+
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < coloms; j++)
+        {
+            ans[i,j]=0;
+
+            for (int k = 0; k < universal; k++)
+            {
+                ans[i,j]+= first[i,k]*second[k,j];
+            }
+        }
+    }
+
+    return ans;
+}
---
+++ Dz_Zadacha 60/Program.cs
@@ -0,0 +1,83 @@
+﻿// *Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int maximum = 28;
+int minimum = 20;
+int range = (maximum - minimum+1);
+
+
+int[,,] matrix = CreateMatrix(2, 2, 2, minimum, maximum);
+
+PrintMatrix(matrix);
+
+
+
+
+int[,,] CreateMatrix(int x, int y, int z, int min, int max )
+{
+    int count=0;//////
+    int[] array = new int[range];
+    int[,,] mtr = new int[x, y, z];
+
+    for (int i = 0; i < mtr.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtr.GetLength(1); j++)
+        {
+            for (int k = 0; k < mtr.GetLength(2); k++)
+            {
+
+                array[count] = CheckRepit(min, max, array, count);
+                mtr[i, j, k] = array[count];
+                
+                count++;
+
+
+            }
+        }
+    }
+  
+    return mtr;
+}
+
+
+int CheckRepit(int min, int max, int[] arr, int co)
+{
+    Random rnd = new Random();
+    int a = 0;
+    bool rep = true;
+
+
+while (rep==true)
+{
+    a = rnd.Next(min,max+1);
+    rep = false;
+
+    for (int i = 0; i < co+1; i++)
+    { if (arr[i] == a) {rep=true;}}
+
+}
+
+return a;
+}
+
+
+void PrintMatrix(int[,,] mtr)
+{
+    for (int i = 0; i < mtr.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtr.GetLength(1); j++)
+        {
+            for (int k = 0; k < mtr.GetLength(2); k++)
+            {
+                Console.Write($"{mtr[i, j, k]} {(i, j, k)},  ");
+            }
+        }
+    }
+    Console.WriteLine($"\b\b\b. ");
+
+}
