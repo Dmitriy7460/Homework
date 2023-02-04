b312f8357c506c1e392c690c0528db6952ab6ba3
Дмитрий <matveev_dmitriy1996@mail.ru>
2023-02-04 06:02:40 +0300

    fix

---
+++ Dz9_Zadacha 64/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+int number =InPut();
+Recursion(number);
+
+
+/////////
+
+int InPut ()
+{
+    Console.Write("Введи число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void Recursion (int num)
+{
+    Console.Write($"{num}, ");
+
+  if (num>1) Recursion (--num);
+  else Console.WriteLine($"\b\b.");
+}
+
+
+
+
+
+
---
+++ Dz9_Zadacha 66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+Console.Write("Введи число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число 2 : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int rezult = number1;
+int summa = number1;
+
+SumOfArray(number1, number2, rezult, summa);
+
+//\\//\\//\\//
+
+void SumOfArray(int num1, int num2, int rez, int sum)//
+{
+    int universal = (num2 - num1) / (Math.Abs(num2 - num1));
+
+    if (rez == num2)
+    {
+        Console.WriteLine($"Cумма натуральных элементов в промежутке от {num1} до {num2} = {sum }  ");
+        return;
+    }
+    
+    SumOfArray(num1, num2, rez += universal, sum += rez);
+}
