33b61a3ae2b22eaaf8c0591372a3b38c630f0c4e
Дмитрий <matveev_dmitriy1996@mail.ru>
2023-02-05 16:48:04 +0300

    Complite homework #9

--- Dz9_Zadacha 66/Program.cs
+++ Dz9_Zadacha 66/Program.cs
@@ -13,21 +13,22 @@ Console.Write("Введи число 2 : ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 int rezult = number1;
-int summa = number1;
 
-SumOfArray(number1, number2, rezult, summa);
 
-//\\//\\//\\//
 
-void SumOfArray(int num1, int num2, int rez, int sum)//
+Console.WriteLine($"Cумма натуральных элементов в промежутке от {number1} до {number2} = {SumOfArray(number1,number2,rezult) }  ");
+     
+
+
+//\\//\\//\\//\\
+
+int SumOfArray(int num1, int num2, int rez)
 {
-    int universal = (num2 - num1) / (Math.Abs(num2 - num1));
-
-    if (rez == num2)
-    {
-        Console.WriteLine($"Cумма натуральных элементов в промежутке от {num1} до {num2} = {sum }  ");
-        return;
-    }
+     if (rez == num2)  return num2;
     
-    SumOfArray(num1, num2, rez += universal, sum += rez);
+    else  
+       { 
+        int universal = (num2 - num1) / (Math.Abs(num2 - num1));
+        return rez + SumOfArray(num1, num2, rez += universal);
+      }  
 }
---
+++ Dz9_Zadacha 68/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("A[m,n]");
+
+Console.Write("Введи m: ");
+int m1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи n: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+
+int answer = 0;
+Console.Write($"{Akker(m1, n2)}");
+
+
+//\\//\\//\\//\\
+
+int Akker(int m, int n)
+{
+    if (m == 0) return ++n;
+    else if (n == 0) return Akker(--m, 1);
+
+    else return Akker(m - 1, Akker(m, --n));
+}
